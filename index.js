a3d192f199a56907bd44c1122b0c7444a65f1ce4
AnyueWang <moonwong.way@gmail.com>
2024-03-29 17:54:33 +0100

    Add function of varying grid size version 1: Add function of changing grid size. Range: 10 to 100.

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
-let nGridPerSide = 16;
+const nGridShow = document.querySelector("#nGrids-show");
+const nGridInput = document.querySelector("#nGrids");
+let nGridPerSide = nGridInput.value;
 const paintArea = document.querySelector(".paint-area");
 
-for (let row = 0; row < nGridPerSide; row++) {
-    for (let col = 0; col < nGridPerSide; col++) {
-        const aPixel = document.createElement("div");
-        const widthInPercentage = 1 / nGridPerSide;
+initilizeGridArea();
 
-        aPixel.classList.add("grid");
-        aPixel.style.width = `${widthInPercentage * 100}%`;
-        aPixel.style.height = `${widthInPercentage * 100}%`;
-
-        paintArea.appendChild(aPixel);
-    }
-}
-
-const btnClear = document.querySelector("#btn-clear");
-const grids = document.querySelectorAll(".grid");
-btnClear.addEventListener("click", () => {
-    grids.forEach((eachGrid) => { eachGrid.style.backgroundColor = "white"; });
+nGridInput.addEventListener("input", (e) => {
+    nGridShow.textContent = e.target.value;
+    nGridPerSide = nGridInput.value;
+    paintArea.replaceChildren();
+    initilizeGridArea();
 });
 
-grids.forEach((eachGrid) => {
-    eachGrid.addEventListener("mouseover", () => { eachGrid.style.backgroundColor = "black"; });
-})
+function initilizeGridArea() {
+    for (let row = 0; row < nGridPerSide; row++) {
+        for (let col = 0; col < nGridPerSide; col++) {
+            const aPixel = document.createElement("div");
+            const widthInPercentage = 1 / nGridPerSide;
+
+            aPixel.classList.add("grid");
+            aPixel.style.width = `${widthInPercentage * 100}%`;
+            aPixel.style.height = `${widthInPercentage * 100}%`;
+
+            paintArea.appendChild(aPixel);
+        }
+    }
+
+    const btnClear = document.querySelector("#btn-clear");
+    const grids = document.querySelectorAll(".grid");
+    
+    btnClear.addEventListener("click", () => {
+        grids.forEach((eachGrid) => { eachGrid.style.backgroundColor = "white"; });
+    });
+    
+    grids.forEach((eachGrid) => {
+        eachGrid.addEventListener("mouseover", () => { eachGrid.style.backgroundColor = "black"; });
+    });
+}
