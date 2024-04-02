568f8e960ab3ec1574058297f43665b4debb3262
AnyueWang <moonwong.way@gmail.com>
2024-04-02 12:58:42 +0200

    Add function of randomized color version 4: The color can be randomized. While color randomization is enabled, the selection of a specific color is disabled.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const nGridShow = document.querySelector("#nGrids-show");
 const nGridInput = document.querySelector("#nGrids");
-let nGridPerSide = nGridInput.value;
 const paintArea = document.querySelector(".paint-area");
+let nGridPerSide = nGridInput.value;
 let drawToggle = false;
+let paintColor = "black";
+let randomColorToggle = false;
 
 initilizeGridArea();
 
@@ -37,22 +39,48 @@ btnClear.addEventListener("click", () => {
     updateInstruction();
 });
 
-let paintColor = "black";
-document.querySelector("#paint-color").addEventListener("input", (e)=>{
+document.querySelector("#paint-color").addEventListener("input", (e) => {
     paintColor = e.target.value;
     console.log(paintColor);
-})
+});
 
 document.addEventListener("keydown", (e) => {
     if (e.key === "s") {
         drawToggle = !drawToggle;
         updateInstruction();
         document.querySelectorAll(".grid").forEach((eachGrid) => {
-            eachGrid.addEventListener("mouseover", () => {if (drawToggle) { eachGrid.style.backgroundColor = paintColor; }});
+            eachGrid.addEventListener("mouseover", () => {
+                if (drawToggle) {
+                    eachGrid.style.backgroundColor = randomColorToggle ? randomColorGenerator() : paintColor;
+                }
+            });
         });
+    }
+});
+
+document.querySelector("#random-color").addEventListener("click", () => {
+    randomColorToggle = !randomColorToggle;
+    const btnRandomColor = document.querySelector("#random-color");
+    const inputPaintColor = document.querySelector("#paint-color");
+    if (randomColorToggle) {
+        btnRandomColor.style.backgroundColor = "#A391A5";
+        inputPaintColor.setAttribute("disabled", "");
+    } else {
+        btnRandomColor.style.backgroundColor = "#577A71";
+        inputPaintColor.removeAttribute("disabled");
     }
 })
 
 function updateInstruction() {
     document.querySelector("#instruction").textContent = drawToggle ? "stop" : "start";
 }
+
+function randomColorGenerator() {
+    const colorDict = "0123456789ABCDEF";
+    let randomColor = "";
+    for (let i = 0; i < 6; i++) {
+        randomColor += colorDict[Math.floor(Math.random() * 16)];
+    }
+    return "#" + randomColor;
+}
+
